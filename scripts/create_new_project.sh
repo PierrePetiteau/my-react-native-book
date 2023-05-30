a51cb6eff8f74bac0050ee81a14529e8a15a3b68
PierrePetiteau <104726314+PierrePetiteau@users.noreply.github.com>
2023-05-30 19:36:14 +0200

    Update script to generate new react native project

--- scripts/create_new_project.sh
+++ scripts/create_new_project.sh
@@ -1,3 +1,29 @@
-npx create-expo-app -t expo-template-blank-typescript
+#!/bin/bash
 
-cd nativewind-app
+if [ -z "$1" ]; then
+  echo "Error: Please provide an app name as a parameter."
+  exit 1
+fi
+
+app_name=$1
+
+echo "Creating a new bare React Native app with Expo and TypeScript..."
+
+# Initialize a new Expo project with the expo-template-blank-typescript template
+npx create-expo-app $app_name --template expo-template-blank-typescript
+
+# Navigate into the project directory
+cd $app_name
+
+npx expo install typescript
+
+# Rename the entry file to TypeScript
+mv App.js App.tsx
+
+# Create ios and android folders
+npx expo prebuild -p all
+
+# Open the project in your preferred code editor
+code .
+
+echo "Done! Your new bare React Native app with Expo and TypeScript ($app_name) is ready."
